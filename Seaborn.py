0cf13d762cecd5a6d3708429703b5ae8c12d931c
Isabel Gonçalves <93488292+bellDataSc@users.noreply.github.com>
2025-02-07 14:15:21 -0300

    Versão com 'warninigs' Seaborn.py
    #Essa opção que será removida em versões futuras do Pandas
    #Para silenciar o aviso add 'warnings'

--- Seaborn.py
+++ Seaborn.py
@@ -1,13 +1,20 @@
-import seaborn as sns
-import pandas as pd
+#Essa opção que será removida em versões futuras do Pandas
+#Para silenciar o aviso add 'warnings'
 
-# Dados de exemplo
-data = pd.DataFrame({
-    'x': [1, 2, 3, 4, 5],
-    'y': [10, 20, 25, 30, 40]
-})
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt  # Importando matplotlib
+
+# Criar um DataFrame de exemplo
+data = pd.DataFrame({'x': [1, 2, 3, 4, 5], 'y': [10, 20, 25, 30, 50]})
 
 # Criar um gráfico
 sns.lineplot(data=data, x='x', y='y', marker='o')
+
+# Adicionar título e exibir o gráfico
 plt.title("Gráfico de Linha com Seaborn")
-plt.show()  # Exibe o gráfico
+plt.show()
